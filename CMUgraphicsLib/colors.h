1910e2aecff2dd71e0170b66a6f6dba3f210e774
ehab <ehab@DESKTOP-6K3F9V9>
2024-12-02 01:30:40 +0200

    added pics folder and ladder color to fix compiler error

--- CMUgraphicsLib/colors.h
+++ CMUgraphicsLib/colors.h
@@ -181,6 +181,6 @@ const color DARKCYAN = color(0, 139, 139);
 const color DARKMAGENTA = color(139, 0, 139);
 const color DARKRED = color(139, 0, 0);
 const color LIGHTGREEN = color(144, 238, 144);
-
+const color Wood = color(86, 50, 50);
 
 #endif //COLOR_H
