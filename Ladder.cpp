dcb98251e94427e8ee113f0e3cfe2b9af5cb09a9
George Ibrahim Aziz <103423847+georgeibrahim1@users.noreply.github.com>
2024-12-13 14:21:22 +0200

    Fixed an error

--- Ladder.cpp
+++ Ladder.cpp
@@ -40,10 +40,7 @@ void Ladder::Apply(Grid* pGrid, Player* pPlayer)
 	//    Review the "pGrid" functions and decide which function can be used for that
 	   
 	pGrid->PrintErrorMessage("You have reached a ladder. Click to continue ...");
-    pGrid->UpdatePlayerCell(pPlayer, endCellPos);
-
-	GameObject * AnotherGameObject = endCellPos.
-	
+    pGrid->UpdatePlayerCell(pPlayer, endCellPos);	
 }
 
 CellPosition Ladder::GetEndPosition() const
