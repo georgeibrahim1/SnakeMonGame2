ce4a610e33df94692961fa849f9b77c1d1b21868
George Ibrahim Aziz <103423847+georgeibrahim1@users.noreply.github.com>
2024-12-18 12:37:56 +0200

    Update SaveGridAction.h

--- SaveGridAction.h
+++ SaveGridAction.h
@@ -2,5 +2,13 @@
 #include "Action.h"
 class SaveGridAction : public Action
 {
-};
+private:
+	string fileName;
 
+
+public:
+	SaveGridAction(ApplicationManager* pApp);
+	virtual void ReadActionParameters();
+	virtual void Execute();
+	virtual ~SaveGridAction();
+};
